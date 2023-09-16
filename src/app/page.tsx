147b62fe83617836f78472f4b4bbeeeb176853eb
Ngounou Ngoubeyou bony junior <nbj.ngoube@gmail.com>
2023-09-16 11:46:18 +0100

    homepage footer builded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
+import { faSquareFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
 export default function Home() {
+  // async function getMoviesData(){
+  //   const res = await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1")
+  // }
+
   return (
-    <>
+    <div className="font-bold flex flex-col h-screen">
       <header id="hero_header" className=""></header>
       <main id="movies">
-        <section></section>
+        <section className="grid gap-4 grid-cols-4">
+          <div className="col-span-1">{/* <Image src={"https://picsumlorem.com/400"}  /> */}</div>
+        </section>
       </main>
 
-      <footer></footer>
-    </>
+      <footer className="flex flex-col items-center pb-14 w-full space-y-4 mt-auto ">
+        <div className=" flex gap-10 justify-center">
+          <FontAwesomeIcon width={20} icon={faSquareFacebook} />
+          <FontAwesomeIcon width={20} icon={faInstagram} />
+          <FontAwesomeIcon width={20} icon={faTwitter} />
+          <FontAwesomeIcon width={20} icon={faYoutube} />
+        </div>
+        <div className=" flex gap-10 justify-center">
+          <span>Conditions of Use</span>
+          <span>Privacy & Policy</span>
+          <span>Press Room</span>
+        </div>
+        <div className="text-center text-gray-500">© 2021 MovieBox by Adriana Eka Prayudha</div>
+      </footer>
+    </div>
   );
 }
